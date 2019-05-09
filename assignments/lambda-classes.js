4f97e3479dd6e1f129e6e39c979a18ecb955ec1d
erozonachi <erozonachi.james@gmail.com>
2019-05-09 13:18:45 +0100

    Create person class
    Wrote Person class with properties; name, age, gender and location,
    and has method; speak().

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,15 @@
 // CODE here for your Lambda Classes
+
+// Person :- The generic class
+class Person {
+  constructor(info) {
+    this.name = info.name;
+    this.age = info.age;
+    this.gender = info.gender;
+    this.location = info.location;
+  }
+
+  speak() {
+    console.log(`Hello my name is ${this.name}, I am from ${this.location}`);
+  }
+}
